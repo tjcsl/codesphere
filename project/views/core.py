f35c404b6ec3c8a49c8df25ca63c037525b29e59
csssuf <csssuf724@gmail.com>
2014-03-29 18:11:01 -0400

    change parameter name

--- project/views/core.py
+++ project/views/core.py
@@ -17,19 +17,19 @@ def login():
 		return redirect('/')
 
 @ghobject.authorized_handler
-def ghcallback(oauth_token):
+def ghcallback(code):
 	next_url = request.args.get('next') or url_for('index')
-	if oauth_token is None:
+	if code is None:
 		flash("Authorization failed.")
 		return redirect(next_url)
 
-	user = User.query.filter_by(github_access_token=oauth_token).first()
+	user = User.query.filter_by(github_access_token=code).first()
 	if user is None:
 		udata = json.loads(ghobject.get('user'))
 		user = User(email=udata['email'], username=udata['login'], github_access_token=github_access_token)
 		user.save()
 
-	user.github_access_token = oauth_token
+	user.github_access_token = code
 	user.save()
 	session['user_id'] = user.username
 	return redirect(next_url)

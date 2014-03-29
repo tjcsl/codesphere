280ca17b90bbbcd0c7da05aa52b9db8fb26b4acc
bobtwinkles <srkoser+GitHub@gmail.com>
2014-03-29 17:12:18 -0400

    Make the project page work

--- project/views/bugs.py
+++ project/views/bugs.py
@@ -1,9 +1,8 @@
 from flask import render_template
-from project.models import Bug
+from project.models import Bug, Project, User
 from project.database import db_session
 
 def browse(owner, project):
-    owner_id = db_session.query(User).filter(User.username == owner).one()
-    project_id = db_session.query(Project).filter(Project.
-    bugs = db_session.query(Bug).filter(Bug.project == project).llimit(20).all()
+#    bugs = db_session.query(Bug).join(Project, User.id == Project.owner).filter(User.username == owner, Project.name == project)
+    bugs = db_session.query(Bug,Project,User).filter(User.username == owner, Project.name == project)
     return render_template("bugs/browse.html", bugs=bugs)

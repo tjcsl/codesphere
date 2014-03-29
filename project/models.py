95b8fe4f47d66d446922a8edeb80ec9fac9afdb8
fwilson <fwilson@fwilson.me>
2014-03-29 14:08:58 -0400

    many models, wow!

---
+++ project/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+from project.database import Base
+
+class ShortenedURL(Base):
+    __tablename__ = 'urls'
+    id = Column(Integer, primary_key=True)
+    short = Column(String(20), unique=True)
+    longer = column(String(512))
+
+    def __init__(self, short=None, longer=None):
+        self.short = short
+        self.longer = longer
+
+    def __repr__(self):
+        return '<ShortURL %s=%s>' % (self.short, self.longer)

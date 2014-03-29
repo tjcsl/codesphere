87d36816b0891c11422c6f88a5c563d4eae4ccbf
csssuf <csssuf724@gmail.com>
2014-03-29 18:27:28 -0400

    fix the impossible syntax error

--- project/__init__.py
+++ project/__init__.py
@@ -4,6 +4,6 @@ app = Flask("project")
 app.config['GITHUB_CLIENT_ID'] = os.environ['GITHUB_CLIENT_ID']
 app.config['GITHUB_CLIENT_SECRET'] = os.environ['GITHUB_CLIENT_SECRET']
 app.config['GITHUB_CALLBACK_URL'] = os.environ['GITHUB_CALLBACK_URL']
-import logging(logLevel=logging.DEBUG)
-logging.basicConfig
+import logging
+logging.basicConfig(logLevel=logging.DEBUG)
 import project.routes

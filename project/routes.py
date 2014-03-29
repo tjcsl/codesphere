c3e25737ad7ce7d7dfc1c0868d3f68c44d790d75
bobtwinkles <srkoser+GitHub@gmail.com>
2014-03-29 15:44:32 -0400

    Fix derps

--- project/routes.py
+++ project/routes.py
@@ -7,5 +7,5 @@ def add_url_routes(routes_tuple):
 
 add_url_routes((
     ('/', core.index),
-    ('/bugs/', bugs.browse)
+    ('/<owner>/<project>/bugs/', bugs.browse)
 ))
--- project/views/bugs.py
+++ project/views/bugs.py
@@ -2,6 +2,6 @@ from flask import render_template
 from project.models import Bug
 from project.database import db_session
 
-def browse():
+def browse(owner, project):
     bugs = db_session.query(Bug).limit(20).all()
-    return render_template("bugs/browse.html", bugs=bugs))
+    return render_template("bugs/browse.html", bugs=bugs)

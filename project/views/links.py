da910933806433121d294021be08aa1aee3ad3bd
csssuf <csssuf724@gmail.com>
2014-03-30 05:51:15 -0400

    Add link shortening (but not retrieval of shortened links)

--- project/views/links.py
+++ project/views/links.py
@@ -1,6 +1,23 @@
 from ..models import ShortenedURL
-from flask import redirect
+from flask import redirect, request, session, render_template
+from ..utils.auth import login_required
 
 def resolve(id):
     resolved = ShortenedURL.query.filter(ShortenedURL.short == id).one()
     return redirect(resolved.longer)
+
+@login_required
+def shortener():
+	if request.method == 'POST':
+		if not 'short' in request.form or not 'long' in request.form:
+			flash("Both fields are required.")
+			return render_template('shortener.html')
+		existing = ShortenedURL.query.filter(ShortenedURL.short == request.form['short']).first()
+		if existing is None:
+			nshort = ShortenedURL(short=request.form['short'], longer=request.form['long'])
+			return redirect('/p/%s' % request.form['short'])
+		else:
+			flash("Sorry, but that short link is already taken.")
+			return render_template('shortener.html')
+	else:
+		return render_template('shortener.html')

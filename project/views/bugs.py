5ac1444fa3301f5db7e71230ffbea4bf4e4c9917
bobtwinkles <srkoser+GitHub@gmail.com>
2014-03-30 05:25:39 -0400

    Add begining routes/templates for bug creation

--- project/views/bugs.py
+++ project/views/bugs.py
@@ -6,7 +6,7 @@ def browse(user, project):
     bugs = db_session.query(Bug).join(Project).join(User).filter(User.username == user, Project.name == project)
     return render_template("bugs/browse.html", bugs=bugs)
 
-def new (user, project):
+def new(user, project):
     return render_template("bugs/new.html", user=user, project=project)
 
 def submit(user, project):

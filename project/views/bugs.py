fea140582c52a33434f47f415918f1213b6eac7b
bobtwinkles <srkoser+GitHub@gmail.com>
2014-03-29 22:13:36 -0400

    Clean up routes

--- project/views/bugs.py
+++ project/views/bugs.py
@@ -2,6 +2,6 @@ from flask import render_template
 from ..models import Bug, Project, User
 from ..database import db_session
 
-def browse(owner, project):
-    bugs = db_session.query(Bug,Project,User).filter(User.username == owner, Project.name == project)
+def browse(user, project):
+    bugs = db_session.query(Bug,Project,User).filter(User.username == user, Project.name == project)
     return render_template("bugs/browse.html", bugs=bugs)

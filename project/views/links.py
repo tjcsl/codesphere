4819bf01ca0d848e927dd5e4e756a210c31a3ee0
csssuf <csssuf724@gmail.com>
2014-03-30 06:12:41 -0400

    Improvements to link shortener - actual checking, flashes, better redirects

--- project/views/links.py
+++ project/views/links.py
@@ -1,5 +1,5 @@
 from ..models import ShortenedURL
-from flask import redirect, request, session, render_template
+from flask import redirect, request, session, render_template, flash
 from ..utils.auth import login_required
 from ..database import db_session
 
@@ -10,17 +10,15 @@ def resolve(id):
 @login_required
 def shortener():
 	if request.method == 'POST':
-		if not 'short' in request.form or not 'long' in request.form:
-			flash("Both fields are required.")
-			return render_template('shortener.html')
+		if request.form['short'] == '' or request.form['long'] == '':
+			flash("Both fields are required.", "danger")
 		existing = ShortenedURL.query.filter(ShortenedURL.short == request.form['short']).first()
 		if existing is None:
 			nshort = ShortenedURL(short=request.form['short'], longer="/u/%s/projects/%s" % (session['username'], request.form['long']))
 			db_session.add(nshort)
 			db_session.commit()
-			return redirect('/p/%s' % request.form['short'])
+			flash("Your link was shortened: http://www.codesphere.org/p/%s" % (request.form['short']), "success")
+			#return redirect('/p/%s' % request.form['short'])
 		else:
-			flash("Sorry, but that short link is already taken.")
-			return render_template('shortener.html')
-	else:
-		return render_template('shortener.html')
+			flash("Sorry, but that short link is already taken.", "danger")
+	return render_template('shortener.html')

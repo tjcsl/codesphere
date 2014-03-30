651895156d6d6df332efced85f8a11e4c6a561da
fwilson <fwilson@fwilson.me>
2014-03-29 20:54:57 -0400

    integers ports

--- web.py
+++ web.py
@@ -9,4 +9,4 @@ handler = RotatingFileHandler("bar.log", maxBytes=1000000)
 handler.setLevel(logging.DEBUG)
 app.logger.addHandler(handler)
 if __name__ == '__main__':
-    app.run(port=getenv("PORT", 5000), debug=True)
+    app.run(port=int(getenv("PORT", 5000)), debug=True)

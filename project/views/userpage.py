41cf47dc90b7506252587bd3a4f2e339b0a80a24
csssuf <csssuf724@gmail.com>
2014-03-29 22:27:32 -0400

    add user repo list view

--- project/views/userpage.py
+++ project/views/userpage.py
@@ -1,4 +1,11 @@
 from flask import render_template
+from ..utils import ghobject
+from ..database import db_session
+from ..models import Project, User
 
 def get_userpage(user):
-    return render_template('userpage.html')
+	asdf = user
+	iprojects = db_session.query(Project, User).filter(User.username == asdf).all()
+	allprojects = ghobject.get('user/repos')
+	print str(allprojects)
+	return render_template('userpage.html', irepos=iprojects)

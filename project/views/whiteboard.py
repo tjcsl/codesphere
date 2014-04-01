e03bb3bdb4f0c520940d63faa82e9c0214b4afe8
csssuf <csssuf724@gmail.com>
2014-04-01 13:30:04 -0400

    begin cleaning up whiteboard variables

--- project/views/whiteboard.py
+++ project/views/whiteboard.py
@@ -4,32 +4,32 @@ from ..database import db_session
 import json
 
 def view_notes(user, project):
-    the_zeroth_thing = db_session.query(User).filter(User.username == user).first().id
-    the_first_thing = db_session.query(Project).filter(Project.owner == the_zeroth_thing, Project.name == project).first().id
-    the_second_thing = db_session.query(WhiteboardNotes).filter(WhiteboardNotes.project == the_first_thing).first()
-    if the_second_thing is not None:
-        the_third_thing = the_second_thing
+    curruser = db_session.query(User).filter(User.username == user).first().id
+    currproject = db_session.query(Project).filter(Project.owner == curruser, Project.name == project).first().id
+    storedwhiteboard = db_session.query(WhiteboardNotes).filter(WhiteboardNotes.project == currproject).first()
+    if storedwhiteboard is not None:
+        newwhiteboard = storedwhiteboard
     else:
-        the_third_thing = WhiteboardNotes(the_first_thing)
-        db_session.add(the_third_thing)
+        newwhiteboard = WhiteboardNotes(currproject)
+        db_session.add(newwhiteboard)
         db_session.commit()
-    return render_template("notes/view.html", hueid=the_first_thing, notes=json.loads(the_third_thing.tasks), thing=the_third_thing, user=user, project=project)
+    return render_template("notes/view.html", hueid=currproject, notes=json.loads(newwhiteboard.tasks), thing=newwhiteboard, user=user, project=project)
 
 def add_task(pid):
-    thing = db_session.query(WhiteboardNotes).filter(WhiteboardNotes.project == pid).first()
-    x = json.loads(thing.tasks)
-    x.append(request.form["task"])
-    thing.tasks = json.dumps(x)
+    currwhiteboard = db_session.query(WhiteboardNotes).filter(WhiteboardNotes.project == pid).first()
+    storedtasks = json.loads(currwhiteboard.tasks)
+    storedtasks.append(request.form["task"])
+    currwhiteboard.tasks = json.dumps(storedtasks)
     db_session.commit()
     stuff = db_session.query(Project).filter(Project.id == pid).first()
     ownerpls = db_session.query(User).filter(User.id == stuff.owner).first()
     return redirect(url_for('view_notes', user=ownerpls.username, project=stuff.name))
 
 def del_task(pid):
-    thing = db_session.query(WhiteboardNotes).filter(WhiteboardNotes.project == pid).first()
-    x = json.loads(thing.tasks)
-    x = [i for i in x if i != request.form["task"]]
-    thing.tasks = json.dumps(x)
+    currwhiteboard = db_session.query(WhiteboardNotes).filter(WhiteboardNotes.project == pid).first()
+    storedtasks = json.loads(currwhiteboard.tasks)
+    storedtasks = [i for i in storedtasks if i != request.form["task"]]
+    currwhiteboard.tasks = json.dumps(storedtasks)
     db_session.commit()
     stuff = db_session.query(Project).filter(Project.id == pid).first()
     ownerpls = db_session.query(User).filter(User.id == stuff.owner).first()
